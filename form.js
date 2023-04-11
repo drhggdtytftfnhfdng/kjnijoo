7927a2bfcd866b8f953254fbd70798e7abb5fda5
drhggdtytftfnhfdng <117073262+drhggdtytftfnhfdng@users.noreply.github.com>
2023-04-11 19:21:42 +0500

    Add files via upload

---
+++ form.js
@@ -0,0 +1,12 @@
+$(document).ready (function () {
+
+ 
+    $(".dws-form").on("click", ".tab", function () {
+        $(".dws-form").find(".active").removeClass('active');
+
+
+        $(this).addClass('active');
+        $(".tab-form").eq($(this).index()).addClass("active")
+    });
+});
+ 
